3340db61e0533814aa831d0df2991bc9a7944cab
chaitanya <gorechaitanya2121@gmail.com>
2024-11-14 11:31:41 +0530

    new project

---
+++ src/books.jsx
@@ -0,0 +1,25 @@
+import img1 from "./images/AtomicHabits.jpg";
+import img2 from "./images/Ddetox.jpg";
+import img3 from "./images/milenia.jpg";
+
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img1,
+    id: 1,
+  },
+
+  {
+    author: "Thibaut Meurisse",
+    title: "Dopamine Detox",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Milenia Trump",
+    title: "Milenia",
+    img: img3,
+    id: 3,
+  },
+];
